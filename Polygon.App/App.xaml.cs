cf5664ffcc14359d28329f01e3683fb53617753e
Grigory Perepechko <grigory.perepechko@corp.badoo.com>
2016-10-02 14:25:34 +0100

    Empty project with references

---
+++ Polygon.App/App.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+
+namespace Polygon.App
+{
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            Bootstrapper bs = new Bootstrapper();
+            bs.Run();
+        }
+
+    }
+}
---
+++ Polygon.App/Bootstrapper.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows;
+using Autofac;
+using Prism.Autofac;
+using Prism.Modularity;
+
+namespace Polygon.App
+{
+    public class Bootstrapper : AutofacBootstrapper
+    {
+        protected override DependencyObject CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void InitializeShell()
+        {
+            Application.Current.MainWindow.Show();
+        }
+
+        protected override void ConfigureModuleCatalog()
+        {
+            ModuleCatalog catalog = (ModuleCatalog)ModuleCatalog;
+        }
+    }
+}
